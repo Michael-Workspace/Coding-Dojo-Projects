84beb78c82526e514b93bcd6cd73e19ecc6d063a
Michael Ngo <mikey.exe.12@gmail.com>
2022-09-30 12:19:44 -0400

    Function Basic I

---
+++ Python v22.1/Fundamentals/Fundamentals/Michael-Ngo-FunctionBasicI/functions_basic_i.py
@@ -0,0 +1,149 @@
+#1
+def number_of_food_groups():
+    return 5
+print(number_of_food_groups())
+# Prediction: 5
+# Actual Result: 5
+
+
+#2
+def number_of_military_branches():
+    return 5
+print(number_of_days_in_a_week_silicon_or_triangle_sides() + number_of_military_branches())
+# Prediction: Error / no value for number_of_days_in_a_week_silicon_or_triangle_sides()
+# Actual Result: NameError: name 'number_of_days_in_a_week_silicon_or_triangle_sides' is not defined
+
+#3
+def number_of_books_on_hold():
+    return 5
+    return 10
+print(number_of_books_on_hold())
+# Prediction:5, 10
+# Actual Result: 5
+
+#4
+def number_of_fingers():
+    return 5
+    print(10)
+print(number_of_fingers())
+# Prediction: 5
+# Actual Result: 5
+
+#5
+def number_of_great_lakes():
+    print(5)
+x = number_of_great_lakes()
+print(x)
+# Prediction: 5, 5
+# Actual Result: 5
+
+#6
+def add(b,c):
+    print(b+c)
+print(add(1,2) + add(2,3))
+# Prediction: Error // no value for b and c in function
+# Actual Result: 3, 5, TypeError: unsupported operand type(s) for +: 'NoneType' and 'NoneType'
+
+#7
+def concatenate(b,c):
+    return str(b)+str(c)
+print(concatenate(2,5))
+# Prediction: 25
+# Actual Result: 25
+
+#8
+def number_of_oceans_or_fingers_or_continents():
+    b = 100
+    print(b)
+    if b < 10:
+        return 5
+    else:
+        return 10
+    return 7
+print(number_of_oceans_or_fingers_or_continents())
+# Prediction: 100, 10, 7
+# Actual Result: 100, 10
+
+#9
+def number_of_days_in_a_week_silicon_or_triangle_sides(b,c):
+    if b<c:
+        return 7
+    else:
+        return 14
+    return 3
+print(number_of_days_in_a_week_silicon_or_triangle_sides(2,3))
+print(number_of_days_in_a_week_silicon_or_triangle_sides(5,3))
+print(number_of_days_in_a_week_silicon_or_triangle_sides(2,3) + number_of_days_in_a_week_silicon_or_triangle_sides(5,3))
+# Prediction:7, 14, 21
+# Actual Result: 7, 14, 21
+
+#10
+def addition(b,c):
+    return b+c
+    return 10
+print(addition(3,5))
+# Prediction: 8
+# Actual Result: 8
+
+#11
+b = 500
+print(b)
+def foobar():
+    b = 300
+    print(b)
+print(b)
+foobar()
+print(b)
+# Prediction: 500, 500, 300, 500
+# Actual Result: 500, 500, 300, 500
+
+#12
+b = 500
+print(b)
+def foobar():
+    b = 300
+    print(b)
+    return b
+print(b)
+foobar()
+print(b)
+# Prediction: 500, 500, 300, 300
+# Actual Result: 500, 500, 300, 500
+
+#13
+b = 500
+print(b)
+def foobar():
+    b = 300
+    print(b)
+    return b
+print(b)
+b=foobar()
+print(b)
+# Prediction: 500, 500, 300, 300
+# Actual Result: 500, 500, 300, 500
+
+#14
+def foo():
+    print(1)
+    bar()
+    print(2)
+def bar():
+    print(3)
+foo()
+# Prediction: 1, 3, 2
+# Actual Result: 1, 3, 2
+
+#15
+def foo():
+    print(1)
+    x = bar()
+    print(x)
+    return 10
+def bar():
+    print(3)
+    return 5
+y = foo()
+print(y)
+# Prediction:1, 3, 5, 10
+# Actual Result: 1, 3, 5, 10
