a58ac8f4edd123457f2f415606852a57bd77f90f
Michael Ngo <mikey.exe.12@gmail.com>
2022-10-31 14:50:37 -0400

    UserCRUD

---
+++ Python v22.1/FLASK_mySQL/DB_connection/Michael-Ngo-UserCRUD/server.py
@@ -0,0 +1,49 @@
+from flask import Flask, render_template, request, redirect
+from users import User
+
+app = Flask(__name__, template_folder = 'templates')
+app.secret_key = "password"
+
+@app.route('/')
+def index():
+    return render_template('result.html',all_users=User.get_all())
+
+@app.route('/new_user')
+def new_user():
+    return render_template('index.html')
+
+@app.route('/create_user',methods=['POST'])
+def create_user():
+    print(request.form)
+    User.save(request.form)
+    return redirect('/')
+
+@app.route ('/show_user/<int:id>')
+def show(id):
+    data = {
+        'id': id
+    }
+    return render_template('/show.html', user=User.get_one(data))
+
+@app.route('/edit_user/<int:id>')
+def edit_user(id):
+    data = {
+        'id':id
+    }
+    return render_template('/edit.html',user=User.get_one(data))
+
+@app.route('/update_user',methods=['POST'])
+def update():
+    User.edit(request.form)
+    return redirect('/')
+
+@app.route('/delete_user/<int:id>')
+def delete_user(id):
+    data = {
+        'id':id
+    }
+    User.delete(data)
+    return redirect ('/')
+
+if __name__ == '__main__':
+    app.run(debug=True)
